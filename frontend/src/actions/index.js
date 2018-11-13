4ae4b48c6cab254823568efc84dc75100ddb29f5
Eugene Krysanov <e.krysanov@edds.ru>
2018-11-13 16:25:01 +0500

    added actions' and reducers' files

---
+++ frontend/src/actions/index.js
@@ -0,0 +1,3 @@
+import * as types from '../constants/ActionTypes'
+
+export const getMeteo = () => ({ type: types.GET_CURRENT_METEO })
---
+++ frontend/src/reducers/Graph.js
@@ -0,0 +1,9 @@
+const initialState = [
+    {
+        "graph": 1
+    }
+]
+
+export default function graph(state = initialState) {
+    return state
+}
