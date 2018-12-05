c176ef1dc514a440d085c2416db8ee651f5db8de
Eugene Krysanov <e.krysanov@edds.ru>
2018-12-05 16:46:10 +0500

    Graph  - add static chart with handle data

--- frontend/src/components/Graph.js
+++ frontend/src/components/Graph.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import graphStore from '../stores/GraphStore'
 import * as Actions from '../actions'
+import { Line } from 'react-chartjs-2'
+import 'chartjs-plugin-annotation'
 
 
 class Graph extends Component {
-    constructor(props) {
-        super(props)
-        this.state = graphStore.initState()
-    }
+    state = graphStore.initState()
 
     componentDidMount() {
         graphStore.on('updateGraph', this.updateState)
@@ -29,14 +28,73 @@ class Graph extends Component {
     }
 
     updateState = () => {
-        this.setState(graphStore.getState)
+        this.setState(graphStore.getState())
     }
 
     render() {
+       const data = {
+            labels: ['11:00', '12:00', '13:00'],
+            datasets: [{
+                label: 'Температура',
+                backgroundColor: 'orangered',
+                borderColor: 'orangered',
+                data: [-10, -15, -14],
+                fill: false,
+            },
+                {
+                    label: 'Ветер',
+                    backgroundColor: 'skyblue',
+                    borderColor: 'skyblue',
+                    data: [1, 2, 0],
+                    fill: false,
+                }]
+        }
+
+        const options = {
+            responsive: true,
+            title: {
+                display: true,
+                text: 'ПОГОДНЫЙ ГРАФИК',
+                fontSize: 32,
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false,
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: true
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'ВРЕМЯ',
+                        fontSize: 32,
+                        fontFamily: 'Roboto',
+                        backgroundColor: 'skyblue',
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'ЗНАЧЕНИЕ',
+                        fontSize: 32,
+                        fontFamily: 'Roboto'
+                    }
+                }]
+            }
+        }
+
         return(
             <section key="1" className="row d-flex flex-wrap justify-content-around align-items-center">
                 <div className="col-sm-11 col-lg-6 graph">
-                    <canvas id="canvas"></canvas>
+                    <Line
+                        data={data}
+                        options={options}
+                    />
                     <span id="chart"></span>
                 </div>
                 <div className="col d-none d-none d-lg-block meteo__block">
