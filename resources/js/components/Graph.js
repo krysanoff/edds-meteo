28a33b17d8f93cd21588dcf575e52e86192c7a4b
Eugene Krysanov <e.krysanov@edds.ru>
2019-01-18 16:12:15 +0500

    - fix graph updating

--- resources/js/components/Graph.js
+++ resources/js/components/Graph.js
@@ -13,13 +13,13 @@ class Graph extends Component {
     componentDidMount() {
         console.log('graph has just mounted')
         graphStore.on('updateGraph', this.updateState)
-        this.intervalId = setInterval(this.updateGraph, this.timeToFullHour())
+        this.setTimeToNextUpdate()
         console.log(this.timeToFullHour())
     }
 
     componentWillUnmount() {
         console.log('graph will unmount')
-        graphStore.removeListener('updateGraph')
+        graphStore.removeListener('updateGraph', this.updateGraph)
     }
 
 
@@ -31,13 +31,12 @@ class Graph extends Component {
 
     componentDidUpdate() {
         console.log('graph has updated')
-        this.intervalId = this.intervalId = setInterval(this.updateGraph, this.timeToFullHour())
+        this.setTimeToNextUpdate()
     }
 
     timeToFullHour() {
         const d = new Date()
-        const h = new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours() + 1, 2, 0, 0)
-        console.log(h - d)
+        const h = new Date(d.getFullYear(), d.getMonth(), d.getDate(), d.getHours(), d.getMinutes()+1, 0, 0)
         return (h - d)
     }
 
@@ -49,70 +48,74 @@ class Graph extends Component {
         this.setState(graphStore.getState())
     }
 
-    graph = {
-        data: {
-            labels: this.state.time,
-            datasets: [{
-                label: 'Температура',
-                backgroundColor: 'orangered',
-                borderColor: 'orangered',
-                data: this.state.temperature,
-                fill: false,
-            },
-                {
-                    label: 'Ветер',
-                    backgroundColor: 'skyblue',
-                    borderColor: 'skyblue',
-                    data: this.state.wind,
-                    fill: false,
-                }]
-        },
-        options: {
-            responsive: true,
-            title: {
-                display: true,
-                text: 'ПОГОДНЫЙ ГРАФИК',
-                fontSize: 32,
-            },
-            tooltips: {
-                mode: 'index',
-                intersect: false,
-            },
-            hover: {
-                mode: 'nearest',
-                intersect: true
-            },
-            scales: {
-                xAxes: [{
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'ВРЕМЯ',
-                        fontSize: 32,
-                        fontFamily: 'Roboto',
-                        backgroundColor: 'skyblue',
-                    }
-                }],
-                yAxes: [{
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'ЗНАЧЕНИЕ',
-                        fontSize: 32,
-                        fontFamily: 'Roboto'
-                    }
-                }]
-            }
-        }
+    setTimeToNextUpdate() {
+        this.intervalId = setInterval(this.updateGraph, this.timeToFullHour())
     }
 
     render() {
+        let graph = {
+            data: {
+                labels: this.state.time,
+                datasets: [{
+                    label: 'Температура',
+                    backgroundColor: 'orangered',
+                    borderColor: 'orangered',
+                    data: this.state.temperature,
+                    fill: false,
+                },
+                    {
+                        label: 'Ветер',
+                        backgroundColor: 'skyblue',
+                        borderColor: 'skyblue',
+                        data: this.state.wind,
+                        fill: false,
+                    }]
+            },
+            options: {
+                responsive: true,
+                title: {
+                    display: true,
+                    text: 'ПОГОДНЫЙ ГРАФИК',
+                    fontSize: 32,
+                },
+                tooltips: {
+                    mode: 'index',
+                    intersect: false,
+                },
+                hover: {
+                    mode: 'nearest',
+                    intersect: true
+                },
+                scales: {
+                    xAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'ВРЕМЯ',
+                            fontSize: 32,
+                            fontFamily: 'Roboto',
+                            backgroundColor: 'skyblue',
+                        }
+                    }],
+                    yAxes: [{
+                        display: true,
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'ЗНАЧЕНИЕ',
+                            fontSize: 32,
+                            fontFamily: 'Roboto'
+                        }
+                    }]
+                }
+            }
+        }
+
        return(
             <section key="1" className="row d-flex flex-wrap justify-content-around align-items-center">
                 <div className="col-sm-11 col-lg-6 graph">
                     <Line
-                        data={this.graph.data}
-                        options={this.graph.options}
+                        data={graph.data}
+                        options={graph.options}
                     />
                     <span id="chart"></span>
                 </div>
