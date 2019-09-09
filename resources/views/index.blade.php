9067fe74042314951ca4cbc08aa344a159be1ea8
Eugene Krysanov <eugene.krysanov@gmail.com>
2019-09-09 09:19:23 +0500

    - refactor dataset timeout to interval

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div id="meteo" data-meteo='{{ $meteo }}'></div>
     <div id="graph" data-graph='{{ $graph }}'></div>
-    <div id="timeout" data-timeout='{{ env('APP_DATA_UPDATE_TIMEOUT') }}'></div>
+    <div id="timeout" data-interval='{{ env('APP_DATA_UPDATE_INTERVAL') }}'></div>
     <div id="root"></div>
 
     <script src="{{ mix('js/index.js') }}"></script>
